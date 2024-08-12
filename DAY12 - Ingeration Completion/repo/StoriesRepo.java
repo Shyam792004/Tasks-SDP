d7a58e0e446efc5243800b7fa72b453cdbb0429a
Shyam792004 <124516994+Shyam792004@users.noreply.github.com>
2024-08-12 15:46:04 +0530

    Delete

--- DAY12 - Ingeration Completion/repo/StoriesRepo.java
+++
@@ -1,9 +0,0 @@
-package com.littleinn.storytelling.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.littleinn.storytelling.model.Stories;
-
-public interface StoriesRepo extends JpaRepository<Stories, Long> {
-
-}
